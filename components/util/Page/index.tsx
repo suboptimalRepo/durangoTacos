39bc6474c449a5335675c443c592c6834d78f5ca
Mario Lopez <mariodolt@marios-mbp.lan>
2023-02-06 15:35:07 -0800

    add meta

--- components/util/Page/index.tsx
+++ components/util/Page/index.tsx
@@ -1,12 +1,18 @@
 import { ReactNode } from "react";
 import css from "./index.module.css";
+import SEOHeader from "../SEOHeader";
 
 type Props = {
   children?: ReactNode;
 };
 
 const Page = (props: Props): JSX.Element => {
-  return <div className={css.page}>{props.children}</div>;
+  return (
+    <div className={css.page}>
+      <SEOHeader />
+      {props.children}
+    </div>
+  );
 };
 
 export default Page;
