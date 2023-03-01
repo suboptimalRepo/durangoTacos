8230233808fc28c72e85d77b3b42a5ba3b7c1ed0
Mario Lopez <mariodolt@marios-mbp.lan>
2023-02-28 21:08:47 -0800

    rough sections done

--- components/Section/index.tsx
+++ components/Section/index.tsx
@@ -1,30 +1,43 @@
-import Link from "next/link";
-import css from "./index.module.css";
-import cx from "classnames";
+import Section from "../Section/Section";
+import { GiTacos } from "react-icons/gi";
+import { GiFoodTruck } from "react-icons/gi";
+import { IoMdCall } from "react-icons/io";
 
-type Props = {
-  icon: JSX.Element;
-  title: string;
-  description?: string;
-  routeTo: string;
-  routeText: string;
-  swap?: boolean;
-};
-
-const Section = (props: Props): JSX.Element => {
+export const ContactUs = (): JSX.Element => {
   return (
-    <div className={css.container}>
-      <div className={css.title}>
-        <h3>{props.title}</h3>
-        <p>{props.description}</p>
-        <Link href={`/${props.routeTo}`}>
-          <button>{props.routeText}</button>
-        </Link>
-      </div>
-
-      <div className={css.icon}>{props.icon}</div>
-    </div>
+    <Section
+      routeText="Call Us"
+      routeTo="contactUs"
+      description="Need us to cater your next party?"
+      title="Contact Us"
+      icon={<IoMdCall />}
+      swap
+    />
   );
 };
 
-export default Section;
+export const OrderOnline = (): JSX.Element => {
+  return (
+    <Section
+      routeText="Order on Grubhub"
+      routeTo="orderOnline"
+      description="Make any night a taco night"
+      title="Order Online"
+      icon={<GiTacos />}
+      swap
+    />
+  );
+};
+
+export const FindUs = (): JSX.Element => {
+  return (
+    <Section
+      routeText="Find us"
+      routeTo="findUs"
+      description="Need directions or our hours"
+      title="Find us"
+      icon={<GiFoodTruck />}
+      swap
+    />
+  );
+};
--- components/pageComponents/Homepage/index.tsx
+++ components/pageComponents/Homepage/index.tsx
@@ -1,11 +1,12 @@
-import ContactUs from "./ContactUs";
-import EatWithUs from "./EatWithUs";
+import { ContactUs, FindUs, OrderOnline } from "../../Section/index";
 import css from "./index.module.css";
 
 const HomePage = (): JSX.Element => {
   return (
     <main className={css.mainContainer}>
       <ContactUs />
+      <FindUs />
+      <OrderOnline />
       {/* <EatWithUs /> */}
     </main>
   );
